46f6616755ea338a96ccb83000a26264fa85e2bc
Chernov Vasilii <v.chernov@crystals.ru>
2015-12-16 18:57:04 +0300

    tmp 8

--- jobs/example7Jobs.groovy
+++ jobs/example7Jobs.groovy
@@ -12,3 +12,16 @@ new FlexCiJobBuilder(
         antAirSdkDir: "/opt/airsdk",
         antBuildTestFile: ""
 ).build(this)
+
+new FlexCiJobBuilder(
+        name: "new-test-flex-unit-shit",
+        description: "test-new",
+        gitHubCredentials: "31df12ac-5d1f-495d-99fe-ad351505d316",
+        gitHubCheckoutDir: "setretail10",
+        gitHubOwnerAndProject: "crystalservice/setretail10",
+        antBuildFile: "setretail10/SetRetail10_Server_GUI/build.xml",
+        antSourceDir: "setretail10/SetRetail10_Server_GUI",
+        antFlexSdkDir: "/opt/flexsdk",
+        antAirSdkDir: "/opt/airsdk",
+        antBuildTestFile: "setretail10/SetRetail10_Server_GUI/build_tests.xml"
+).build(this)
